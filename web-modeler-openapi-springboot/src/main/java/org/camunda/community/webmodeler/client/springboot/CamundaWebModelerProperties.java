a8285032d60a11fcd1021888f4e75a703c6ab71b
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-04-03 11:12:45 +0200

    adjusted readme

--- web-modeler-openapi-springboot/src/main/java/org/camunda/community/webmodeler/client/springboot/CamundaWebModelerProperties.java
+++ web-modeler-openapi-springboot/src/main/java/org/camunda/community/webmodeler/client/springboot/CamundaWebModelerProperties.java
@@ -1,9 +1,8 @@
 package org.camunda.community.webmodeler.client.springboot;
 
 import io.camunda.identity.sdk.IdentityConfiguration.Type;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.net.URL;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("camunda.modeler.client")
 public record CamundaWebModelerProperties(
